d680c7e35420dcf40e206a7b48ce6491e9727c3e
bernco <chisombernard@gmail.com>
2020-12-13 04:32:56 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from turtle import Turtle, Screen, colormode
+import random
+
+my_turtle = Turtle()
+my_turtle.shape("arrow")
+my_turtle.speed(0)
+my_color_mode = colormode(255)
+direction = [0, 90, 180, 270]
+
+
+def color_maker():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return r, g, b
+
+
+def draw_spiral(gap_size):
+    for _ in range(int(360/gap_size)):
+        my_turtle.circle(100)
+        my_turtle.left(gap_size)
+        my_turtle.pencolor(color_maker())
+
+
+draw_spiral(2)
+
+my_screen = Screen()
+my_screen.exitonclick()
+
